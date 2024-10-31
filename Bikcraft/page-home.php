5fcac1e61b27a685ec5dc02e5c12a9dd507f93e8
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-10-31 16:34:41 -0300

    fix links

--- Bikcraft/page-home.php
+++ Bikcraft/page-home.php
@@ -31,7 +31,7 @@ get_header();
 			<p><?php the_field('quote_introducao'); ?></p>
 			<cite><?php the_field('citacao_introducao'); ?></cite>
 		</blockquote>
-		<a href="/produtos" class="btn">Orçamento</a>
+		<a href="<?php bloginfo('url'); ?>/produtos" class="btn">Orçamento</a>
 	</div>
 </section>
 
@@ -62,7 +62,7 @@ get_header();
 
 	<div class="call">
 		<p><?php the_field('chamada_produtos'); ?></p>
-		<a href="/produtos/" class="btn btn-preto">Produtos</a>
+		<a href="<?php bloginfo('url'); ?>/produtos/" class="btn btn-preto">Produtos</a>
 	</div>
 
 </section>
