6ef2b77916664770ff63560b2cb7e90c0ca5fb5c
Luiz Mateus Sanches de Oliveira <lzu.luizmateus@gmail.com>
2024-12-18 10:26:40 -0300

    fix og:url for new function

--- Bikcraft/header.php
+++ Bikcraft/header.php
@@ -8,7 +8,7 @@
 		<meta property="og:type" content="website"/>
 		<meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
 		<meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>"/>
-		<meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+		<meta property='og:url' content='<?php echo esc_url( home_url() ); ?>'/>
 		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
