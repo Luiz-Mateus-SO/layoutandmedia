ef2e4fbe22cbef36c434c28e3055680c044d7a3f
Luiz Mateus Sanches de Oliveira <lzu.luizmateus@gmail.com>
2024-12-18 10:09:25 -0300

    insert text domain

--- Bikcraft/functions.php
+++ Bikcraft/functions.php
@@ -32,7 +32,7 @@
 
   // Resgistrar Menu
   function register_my_menu() {
-    register_nav_menu('menu-principal',__( 'Menu Principal' ));
+    register_nav_menu('menu-principal', __( 'Menu Principal', 'layout-and-media' ));
   }
   add_action( 'init', 'register_my_menu' );
 
